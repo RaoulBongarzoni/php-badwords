8a509e189b4d51f4c8208af3e7bfa423649e5e60
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2023-12-18 18:05:33 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php $ciaoMondo = "TESTO DA STAMPARE TRAMITE VARIABILE"?>
+</head>
+<body>
+    <h1>hello wordls <?php echo "$ciaoMondo php"?></h1>
+</body>
+</html>
